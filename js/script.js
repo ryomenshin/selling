aa7e356376aeedf277a3175be12b8107d49d5499
WildanNurFauzan <leadergalaxybleh@gmail.com>
2023-06-20 20:03:59 -0500

    Fix js bug

--- js/script.js
+++ js/script.js
@@ -1,18 +1,30 @@
-// Toggle class active
-
+// Toggle class active for menu
 const navbarNav = document.querySelector(".navbar-nav");
-
-// When Menu clicked
-
-document.querySelector("#goodsMenu");
-onclick = () => {
+// When menu clicked
+document.querySelector("#goods-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// Click on the outside to remove menu
+// Toggle class active for search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
 
-const menu = document.querySelector("#goodsMenu");
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Click on the outside of sidebar to remove
+
+const menu = document.querySelector("#goods-menu");
+const sb = document.querySelector("#search-button");
+
 document.addEventListener("click", function (e) {
-  if (!menu.contains(e.target) && !navbarNav.contains(e, target))
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
 });
