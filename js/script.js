345b705a19af2313e845a44fb3641f34779c50f4
WildanNurFauzan <leadergalaxybleh@gmail.com>
2023-06-22 19:14:02 -0500

    Shopping Bag

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
-// Toggle class active for menu
+// Var
 const navbarNav = document.querySelector(".navbar-nav");
-// When menu clicked
-document.querySelector("#goods-menu").onclick = () => {
-  navbarNav.classList.toggle("active");
-};
-
-// Toggle class active for search form
+const shoppingBag = document.querySelector(".shopping-bag");
 const searchForm = document.querySelector(".search-form");
 const searchBox = document.querySelector("#search-box");
+const menu = document.querySelector("#goods-menu");
+const sb = document.querySelector("#search-button");
+const sbb = document.querySelector("#shopping-bag-button");
+
+// Toggle class active
+// When menu clicked
+document.querySelector("#goods-menu").onclick = (e) => {
+  navbarNav.classList.toggle("active");
+  e.preventDefault();
+};
+document.querySelector("#shopping-bag-button").onclick = (e) => {
+  shoppingBag.classList.toggle("active");
+  e.preventDefault();
+};
 
 document.querySelector("#search-button").onclick = (e) => {
   searchForm.classList.toggle("active");
@@ -17,14 +26,14 @@ document.querySelector("#search-button").onclick = (e) => {
 
 // Click on the outside of sidebar to remove
 
-const menu = document.querySelector("#goods-menu");
-const sb = document.querySelector("#search-button");
-
 document.addEventListener("click", function (e) {
   if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
   if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
     searchForm.classList.remove("active");
+  }
+  if (!sbb.contains(e.target) && !shoppingBag.contains(e.target)) {
+    shoppingBag.classList.remove("active");
   }
 });
